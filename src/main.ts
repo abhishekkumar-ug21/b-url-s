b541a9f4fba376022c95b86ca471b4cc93468efb
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-12-17 14:17:21 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   console.log('Port:', process.env.PORT); // Verify PORT variable for debugging
   await app.listen(process.env.PORT ?? 3000);
 }
