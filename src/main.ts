38fb5ce80e53921cd35e2f09a66601ee830c5511
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-12-17 14:33:17 +0530

    now lets host adn check if errors comes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,24 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  const allowedOrigins: string[] = [
+    // 'http://localhost:8084',
+    // 'https://staging.your-domain.com',
+    // 'https://your-domain.com'
+    process.env.LOCAL_ORIGINS,
+    // process.env.NETLIFY_FINAL_ORIGINS,
+    process.env.RANDOM_ORIGINS,
+    process.env.VERCLE_FINAL_ORIGINS,
+    process.env.VERCLE_ORIGINS
+  ];
+  const corsOptions = {
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+  app.enableCors(corsOptions);
 
   console.log('Port:', process.env.PORT); // Verify PORT variable for debugging
   await app.listen(process.env.PORT ?? 3000);
